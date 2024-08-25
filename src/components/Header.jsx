a930baf7eb6c0179bb1f8f59c0c972b098b50d22
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-25 15:46:36 +1000

    Finalised components, tweaked to handle custom usehttp hook & checkout success, more error handling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,29 +5,29 @@ import logoImg from '../assets/logo.jpg';
 import CartContext from './cart/CartContext.jsx';
 import UserProgressContext from './cart/UserProgressContext.jsx';
 
-export default function Header(){
+export default function Header() {
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext);
-
+  
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
-        return totalNumberOfItems + item.quantity;
+      return totalNumberOfItems + item.quantity;
     }, 0);
-
-    function handleShowCart(){
-        userProgressCtx.showCart();
+  
+    function handleShowCart() {
+      userProgressCtx.showCart();
     }
-
-    return(
-        <header id="main-header">
-            <div id="title">
-                <img src={logoImg} alt="A restaurant"/>
-                <h1>ReactFood</h1>
-            </div>
-            <nav>
-                <Button textOnly onClick={handleShowCart}>
-                Cart ({totalCartItems})
-                </Button>
-            </nav>
-        </header>
+  
+    return (
+      <header id="main-header">
+        <div id="title">
+          <img src={logoImg} alt="A restaurant" />
+          <h1>ReactFood</h1>
+        </div>
+        <nav>
+          <Button textOnly onClick={handleShowCart}>
+            Cart ({totalCartItems})
+          </Button>
+        </nav>
+      </header>
     );
-}
+  }
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,27 +1,29 @@
-import { useState, useEffect } from "react";
-
 import MealItem from './MealItem.jsx';
+import useHttp from './hooks/useHttp.js';
+import Error from './Error.jsx';
 
-export default function Meals(){
-    const [loadedMeals, setLoadedMeals] = useState([]);
+const requestConfig = {};
 
-    useEffect(() => {
-        async function fetchMeals(){
-            const response = await fetch('http://localhost:3000/meals');
-    
-            if (!response.ok){
-                //....
-            }
-    
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
-        fetchMeals();
-    }, []);
-    
-    return <ul id="meals">
+export default function Meals() {
+    const {
+      data: loadedMeals,
+      isLoading,
+      error,
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
+  
+    if (isLoading) {
+      return <p className="center">Fetching meals...</p>;
+    }
+  
+    if (error) {
+      return <Error title="Failed to fetch meals" message={error} />;
+    }
+  
+    return (
+      <ul id="meals">
         {loadedMeals.map((meal) => (
-        <MealItem key={meal.id} meal={meal}/>
+          <MealItem key={meal.id} meal={meal} />
         ))}
-        </ul>;
-}
+      </ul>
+    );
+  }
