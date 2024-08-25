123ea54aa2a28f2576bdea2cc52dad0429c41000
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-25 14:17:12 +1000

    Tweaked Cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import Cart from './components/Cart.jsx';
 import Header from './components/Header.jsx';
 import Meals from './components/Meals.jsx';
 import { CartContextProvider } from './components/cart/CartContext.jsx';
+import { UserProgressContextProvider } from './components/cart/UserProgressContext.jsx';
 
 function App() {
   return (
-    <>
+    <UserProgressContextProvider>
     <CartContextProvider>
       <Header />
       <Meals />
+      <Cart />
     </CartContextProvider>
-    </>
+    </UserProgressContextProvider>
   );
 }
 
