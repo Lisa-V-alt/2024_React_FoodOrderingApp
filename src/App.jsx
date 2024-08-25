c87fa899dc877bbbea35199c8e75d88b503e7bd4
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-25 13:09:11 +1000

    added CartContext object and items array

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from './components/Header.jsx';
 import Meals from './components/Meals.jsx';
+import { CartContextProvider } from './components/cart/CartContext.jsx';
 
 function App() {
   return (
     <>
+    <CartContextProvider>
       <Header />
       <Meals />
+    </CartContextProvider>
     </>
   );
 }
